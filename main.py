215abbd233c1c1b636e6465af8faaea388a85f0a
YeomJaemoo <125108268+YeomJaemoo@users.noreply.github.com>
2023-12-16 16:18:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import streamlit as st 
 
 
-st.title("_Reverse Engineering_Helper!_")
+st.title("_🚨Reverse :red[Engineering]_Helper!_")
+st.write("## 튜토리얼을 시청 후 사용하시면 좋습니다:)")
 
 video_url= "https://www.youtube.com/watch?v=0pD1yWTpUOY"
 st.video(video_url, format='video/mp4')
