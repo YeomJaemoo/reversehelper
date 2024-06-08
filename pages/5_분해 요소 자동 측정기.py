d639d9e0d154d480fde39fb122d90f21c4d6f9af
YeomJaemoo <125108268+YeomJaemoo@users.noreply.github.com>
2024-06-08 09:20:17 +0900

    Update 5_분해 요소 자동 측정기.py

--- pages/5_분해 요소 자동 측정기.py
+++ pages/5_분해 요소 자동 측정기.py
@@ -3,83 +3,73 @@ import numpy as np
 import streamlit as st
 from object_detector import *
 import base64
-
 st.title("Measure Object Size")
-st.text("## 오류해결이 필요함.")
+st.text("##오류해결이 필요함.")
 st.text("로컬에서는 잘 처리되나 배포할 때 애러가 남. 스트리밍에 대한 해결 후 배포할 예정!")
+# Load Aruco detector
+parameters = cv2.aruco.DetectorParameters_create()
+aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_50)
 
-# Aruco 모듈을 가져오고 예외를 처리합니다.
-try:
-    parameters = cv2.aruco.DetectorParameters_create()
-    aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_50)
-except AttributeError:
-    st.error("Aruco 모듈을 찾을 수 없습니다. opencv-contrib-python이 설치되어 있는지 확인하세요.")
-
-# 객체 감지기 로드
+# Load Object Detector
 detector = HomogeneousBgDetector()
 
-# 비디오 캡처 로드
+# Load Cap
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
-# 이미지를 표시할 자리 표시자 생성
+
+# Create a placeholder for the image
 image_placeholder = st.empty()
 
-# 다운로드 링크를 표시할 자리 표시자 생성
+# Create a placeholder for the download link
 download_placeholder = st.empty()
 
 capture_button = st.button("캡쳐하기")
 
-# Streamlit 루프
+# Streamlit loop
 while True:
-    ret, img = cap.read()
+    _, img = cap.read()
 
-    if not ret:
-        st.error("카메라 스트림을 읽을 수 없습니다.")
-        break
+    # Get Aruco marker
+    corners, _, _ = cv2.aruco.detectMarkers(img, aruco_dict, parameters=parameters)
+    if corners:
 
-    # Aruco 마커 감지
-    try:
-        corners, _, _ = cv2.aruco.detectMarkers(img, aruco_dict, parameters=parameters)
-        if corners:
-            # 마커 주위에 다각형 그리기
-            int_corners = np.int0(corners)
-            cv2.polylines(img, int_corners, True, (0, 255, 0), 5)
+        # Draw polygon around the marker
+        int_corners = np.int0(corners)
+        cv2.polylines(img, int_corners, True, (0, 255, 0), 5)
 
-            # Aruco 둘레 계산
-            aruco_perimeter = cv2.arcLength(corners[0], True)
+        # Aruco Perimeter
+        aruco_perimeter = cv2.arcLength(corners[0], True)
 
-            # 픽셀을 cm로 변환하는 비율
-            pixel_cm_ratio = aruco_perimeter / 20
+        # Pixel to cm ratio
+        pixel_cm_ratio = aruco_perimeter / 20
 
-            contours = detector.detect_objects(img)
+        contours = detector.detect_objects(img)
 
-            # 객체 경계선 그리기
-            for cnt in contours:
-                # 직사각형 구하기
-                rect = cv2.minAreaRect(cnt)
-                (x, y), (w, h), angle = rect
+        # Draw objects boundaries
+        for cnt in contours:
+            # Get rect
+            rect = cv2.minAreaRect(cnt)
+            (x, y), (w, h), angle = rect
 
-                # 픽셀을 cm로 변환하여 객체의 너비와 높이 구하기
-                object_width = w / pixel_cm_ratio
-                object_height = h / pixel_cm_ratio
+            # Get Width and Height of the Objects by applying the Ratio pixel to cm
+            object_width = w / pixel_cm_ratio
+            object_height = h / pixel_cm_ratio
 
-                # 사각형 표시
-                box = cv2.boxPoints(rect)
-                box = np.int0(box)
+            # Display rectangle
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
 
-                cv2.circle(img, (int(x), int(y)), 5, (0, 0, 255), -1)
-                cv2.polylines(img, [box], True, (255, 0, 0), 2)
-                cv2.putText(img, "너비 {} cm".format(round(object_width, 1)), (int(x - 100), int(y - 20)), cv2.FONT_HERSHEY_PLAIN, 2, (100, 200, 0), 2)
-                cv2.putText(img, "높이 {} cm".format(round(object_height, 1)), (int(x - 100), int(y + 15)), cv2.FONT_HERSHEY_PLAIN, 2, (100, 200, 0), 2)
-    except Exception as e:
-        st.error(f"Aruco 마커 처리 중 오류 발생: {e}")
+            cv2.circle(img, (int(x), int(y)), 5, (0, 0, 255), -1)
+            cv2.polylines(img, [box], True, (255, 0, 0), 2)
+            cv2.putText(img, "Width {} cm".format(round(object_width, 1)), (int(x - 100), int(y - 20)), cv2.FONT_HERSHEY_PLAIN, 2, (100, 200, 0), 2)
+            cv2.putText(img, "Height {} cm".format(round(object_height, 1)), (int(x - 100), int(y + 15)), cv2.FONT_HERSHEY_PLAIN, 2, (100, 200, 0), 2)
 
-    # 이미지 표시
+    # Display the image
     image_placeholder.image(img, channels="BGR", use_column_width=True)
 
-    # '캡쳐하기' 버튼이 클릭되면 현재 프레임을 저장합니다.
+    # If the '캡쳐하기' button is clicked, save the current frame
     if capture_button:
         cv2.imwrite('captured_image.jpg', img)
         with open('captured_image.jpg', "rb") as img_file:
